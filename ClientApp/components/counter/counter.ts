cca006fc86b6e4149deed69c7df47f003b2cd847
Sergey <piteryo7@gmail.com>
2017-12-06 14:55:12 +0300

    The biggest fix in the project

--- ClientApp/components/counter/counter.ts
+++ ClientApp/components/counter/counter.ts
@@ -1,15 +1,11 @@
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
-import Router from '../../router/router'
 
 @Component
 export default class CounterComponent extends Vue {
     currentcount: number = 0;
 
     incrementCounter() {
-        this.currentcount++;
-        if (this.currentcount > 5)
-        Router.replace('counter')    
-        
+        this.currentcount++;       
     }
 }
